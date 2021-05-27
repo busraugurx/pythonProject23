52800f6dbf0661368914c0838393ce5e667660c6
busraugurx <busraugur934@gmail.com>
2021-05-28 01:22:34 +0300

    Initial commit

---
+++ shifting.py
@@ -0,0 +1,5 @@
+print("***SAYILARIN YERİNİ DEĞİŞTİRME***")
+sayı_1=float(input("birinci sayıyı giriniz: "))
+sayı_2=float(input("ikinci sayiyi giriniz: "))
+sayı_1,sayı_2=sayı_2,sayı_1
+print("sayı_1: {},sayı_2: {}" .format(sayı_1,sayı_2))
